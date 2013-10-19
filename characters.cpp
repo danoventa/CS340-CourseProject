9a2b3787be5bcbcbadfa20356ca9ad5d6b057365
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2013-10-18 22:12:15 -0500

    adding characters.h file

---
+++ characters.cpp
@@ -0,0 +1,5 @@
+#include "characters.h"
+
+Characters::Characters()
+{
+}
---
+++ characters.h
@@ -0,0 +1,10 @@
+#ifndef CHARACTERS_H
+#define CHARACTERS_H
+
+class Characters
+{
+public:
+    Characters();
+};
+
+#endif // CHARACTERS_H
