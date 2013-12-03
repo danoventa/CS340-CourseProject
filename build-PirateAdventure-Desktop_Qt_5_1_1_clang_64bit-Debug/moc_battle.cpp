23ae17babb1d8358af538589683e5d55cfb1e8d0
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2013-12-03 01:25:38 -0600

    Tring to work it!

--- build-PirateAdventure-Desktop_Qt_5_1_1_clang_64bit-Debug/moc_battle.cpp
+++ build-PirateAdventure-Desktop_Qt_5_1_1_clang_64bit-Debug/moc_battle.cpp
@@ -1,16 +1,16 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'mainwindow.h'
+** Meta object code from reading C++ file 'battle.h'
 **
 ** Created by: The Qt Meta Object Compiler version 67 (Qt 5.1.1)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../../../../PirateAdventure/mainwindow.h"
+#include "../../../../../../PirateAdventure/battle.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#error "The header file 'battle.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.1.1. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_Battle_t {
-    QByteArrayData data[4];
-    char stringdata[55];
+    QByteArrayData data[5];
+    char stringdata[39];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,12 +30,13 @@ struct qt_meta_stringdata_Battle_t {
 static const qt_meta_stringdata_Battle_t qt_meta_stringdata_Battle = {
     {
 QT_MOC_LITERAL(0, 0, 6),
-QT_MOC_LITERAL(1, 7, 21),
-QT_MOC_LITERAL(2, 29, 0),
-QT_MOC_LITERAL(3, 30, 23)
+QT_MOC_LITERAL(1, 7, 15),
+QT_MOC_LITERAL(2, 23, 0),
+QT_MOC_LITERAL(3, 24, 10),
+QT_MOC_LITERAL(4, 35, 2)
     },
-    "Battle\0on_pushButton_clicked\0\0"
-    "on_pushButton_4_clicked\0"
+    "Battle\0keyReleaseEvent\0\0QKeyEvent*\0"
+    "ke\0"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,7 +46,7 @@ static const uint qt_meta_data_Battle[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       1,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -53,12 +54,10 @@ static const uint qt_meta_data_Battle[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   24,    2, 0x08,
-       3,    0,   25,    2, 0x08,
+       1,    1,   19,    2, 0x08,
 
  // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 3,    4,
 
        0        // eod
 };
@@ -68,16 +67,14 @@ void Battle::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void
     if (_c == QMetaObject::InvokeMetaMethod) {
         Battle *_t = static_cast<Battle *>(_o);
         switch (_id) {
-        case 0: _t->on_pushButton_clicked(); break;
-        case 1: _t->on_pushButton_4_clicked(); break;
+        case 0: _t->keyReleaseEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 const QMetaObject Battle::staticMetaObject = {
-    { &QMainWindow::staticMetaObject, qt_meta_stringdata_Battle.data,
+    { &QDialog::staticMetaObject, qt_meta_stringdata_Battle.data,
       qt_meta_data_Battle,  qt_static_metacall, 0, 0}
 };
 
@@ -92,22 +89,22 @@ void *Battle::qt_metacast(const char *_clname)
     if (!_clname) return 0;
     if (!strcmp(_clname, qt_meta_stringdata_Battle.stringdata))
         return static_cast<void*>(const_cast< Battle*>(this));
-    return QMainWindow::qt_metacast(_clname);
+    return QDialog::qt_metacast(_clname);
 }
 
 int Battle::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
-    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    _id = QDialog::qt_metacall(_c, _id, _a);
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 1)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 1;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 1)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 1;
     }
     return _id;
 }
